d87bdb24a4f32be0a360f9fab592d054cd4f242a
Benjamin Appiah-Brobbey <fanky5g@gmail.com>
2024-04-18 07:58:34 +0200

    implement updated database interface

--- driver.go
+++ driver.go
@@ -2,25 +2,26 @@ package ponzu_driver_postgres
 
 import (
 	"context"
+	"fmt"
 	"github.com/fanky5g/ponzu-driver-postgres/database"
 	"github.com/fanky5g/ponzu-driver-postgres/repository"
 	ponzuDriver "github.com/fanky5g/ponzu/driver"
+	"github.com/fanky5g/ponzu/entities"
 	"github.com/fanky5g/ponzu/models"
+	"github.com/fanky5g/ponzu/tokens"
 	"github.com/jackc/pgx/v5/pgxpool"
-	log "github.com/sirupsen/logrus"
 )
 
 type driver struct {
 	conn         *pgxpool.Pool
-	repositories map[string]ponzuDriver.Repository
+	repositories map[tokens.RepositoryToken]ponzuDriver.Repository
 }
 
-func (database *driver) Get(token string) interface{} {
+func (database *driver) GetRepositoryByToken(token tokens.RepositoryToken) ponzuDriver.Repository {
 	if repo, ok := database.repositories[token]; ok {
 		return repo
 	}
 
-	log.Panicf("Repository %s not found", token)
 	return nil
 }
 
@@ -37,15 +38,21 @@ func New(models []models.ModelInterface) (ponzuDriver.Database, error) {
 		return nil, err
 	}
 
-	repos := make(map[string]ponzuDriver.Repository)
+	repos := make(map[tokens.RepositoryToken]ponzuDriver.Repository)
 	for _, model := range models {
+		entity := model.NewEntity()
+		persistable, ok := entity.(entities.Persistable)
+		if !ok {
+			return nil, fmt.Errorf("entity %T is not persistable", entity)
+		}
+
 		var repo ponzuDriver.Repository
 		repo, err = repository.New(conn, model)
 		if err != nil {
 			return nil, err
 		}
 
-		repos[model.Name()] = repo
+		repos[persistable.GetRepositoryToken()] = repo
 	}
 
 	d := &driver{conn: conn, repositories: repos}
